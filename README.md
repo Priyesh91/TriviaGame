e975b1e389c75dc23a6217d6870605b47ad7a26a
Priyesh <91priyesh@gmail.com>
2018-12-20 22:41:47 -0500

    Psuedo code and made master array question template with choices and answers

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TriviaGame
 Trivia-Game
+
+The project demonstrates use of JS timeintervals/timeouts through timed trivia game questions.
+
+Users can launch the index.html file to interact with the gave preface and once generalized can go use the .js file to see how the code interfaces with the questions.
+
+Please feel free to leave a comment or contact me through github dm's, any feedback is welcomed.
+
+The repository and code in it are maintained by Priyesh Patel
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,61 @@
 var name = "Trivia Game"
 console.log(name);
+
+//Create Variables
+var correct = 0;
+var incorrect = 0;
+//Question bank array with another array in it with answer 
+//-->Master array{Object}
+//---------------->question, answer, choices-->[array]
+
+var questionBank = [{
+    question: "question 1",
+    answer: "A",
+    choices: ["A", "B", "C", "D"]
+  },
+  {
+    question: "question 2",
+    answer: "B",
+    choices: ["A", "B", "C", "D"]
+  },
+  {
+    question: "question 3",
+    answer: "A",
+    choices: ["A", "B", "C", "D"]
+  },
+  {
+    question: "question 4",
+    answer: "C",
+    choices: ["A", "B", "C", "D"]
+  },
+  {
+    question: "question 5",
+    answer: "D",
+    choices: ["A", "B", "C", "D"]
+  },
+]
+
+//function for count down timer
+
+//function for displaying question +  choices 
+
+//function for onclick trigger to move to the next question if timer is running
+
+//function to move questions with interval timer
+
+//Start button to trigger the start of game with first question and count down timer starting
+//correct/incorrect answer logging
+//slideshow with questions
+//End of count down timer will triger game to stop and show stats page
+//Clear stats and variables
+//
+
+
+// * The player will have a limited amount of time to finish the quiz. 
+
+//   * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+// * Don't let the player pick more than one answer per question.
+
+// * Don't forget to include a countdown timer.
+
