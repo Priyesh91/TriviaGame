a914b58232133fa4b63003326121faa49dc22df2
Priyesh <91priyesh@gmail.com>
2018-12-21 23:17:26 -0500

    start button working, timer countdown working, questionScreen showing first question from array with cchoices, .on(click) working on choices.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,42 +2,63 @@ var name = "Trivia Game"
 console.log(name);
 
 //Create Variables
+//score variables
 var correct = 0;
 var incorrect = 0;
 var noAnswer = 0;
-var question = 0;
+// var question = 0;-----------delete
 
+// question variables
+var questionNumber = 0;
+var choiceSelected;
+var answered;
+var correctAns;
+var incorrectAns;
+var noAns;
 //Question bank array with another array in it with answer 
 //-->Master array{Object}
 //---------------->question, answer, choices-->[array]
 
 var questionBank = [{
+    questionNumber: "1",
     question: "question 1",
     answer: "A",
     choices: ["A", "B", "C", "D"]
   },
   {
+    questionNumber: "2",
     question: "question 2",
     answer: "B",
     choices: ["A", "B", "C", "D"]
   },
   {
+    questionNumber: "3",
     question: "question 3",
     answer: "A",
     choices: ["A", "B", "C", "D"]
   },
   {
+    questionNumber: "4",
     question: "question 4",
     answer: "C",
     choices: ["A", "B", "C", "D"]
   },
   {
+    questionNumber: "5",
     question: "question 5",
     answer: "D",
     choices: ["A", "B", "C", "D"]
   },
 ];
-
+console.log(questionBank[0].question);
+console.log(questionBank[0].choices[2]);
+//messageArray
+var messageArray = {
+  correct: "Correct!",
+  incorrect: "Incorrect!",
+  timeup: "Time Up!",
+  lastquestion: "Calculating score..."
+}
 //-----------------------------Start-Button-----------------------
 //Startbutton on screen that will be hidden once triggered and start the questionScreen1
 $("#startTrivia").on("click", function () {
@@ -57,65 +78,88 @@ $("#resetTrivia").on("click", function () {
 //------------------------------TIMER-----------------------------
 // timer variables
 var intervalId;
-var clockRunning = false;
 var time = 20;
 //timer count and display function 
 function startTimer() {
   clearInterval(intervalId);
   setInterval(count, 1000);
+
 }
 
 function count() {
   time--;
-  if (time === 0) {
-    noAnsScreen();
-  }
   $(".timer").text("Time Remaining: " + time);
 }
 //-------------------------------TIMER----------------------------
 
-
-//----------------------blankScreen function--------------
+//-------------------------blankScreen function-------------------
 //Clear stats and variables
 function blankScreen() {
   //since things were not clearing properly added  lines to .empty out all the divs before question screen started
-  $(".questionScreen, .messageScreen, .endScreen").empty();
+  $(".timer, .question, .correctScreen, .incorrectScreen, .noAnsScreen, .endScreen, .choice1, .choice2, .choice3, .choice4").empty();
   // hardreset variables once game has been reset
   correct = 0;
   incorrect = 0;
   noAnswer = 0;
-  question = 0;
+  questionNumber = 0;
   questionScreen();
 };
-//----------------------blankQuestionScreen function--------------
+//-------------------------blankScreen function--------------------
 
+//-------------------------questionScreen function-------------------------------
 function questionScreen() {
-alert("hi");
-
-
-
-
-
-
-startTimer();
-if (time === 0) {
-  noAnsScreen();
-}
-
-function noAnsScreen() {
-  clearInterval(intervalId);
-  time = 20;
   startTimer();
+
+  answered = true;
+  //write question 1 to the page
+  $(".question").html(`<h2>${questionBank[questionNumber].question}<h2>`);
+  //write question 1 choices to the page
+  $(".choice1").attr({
+    "data-index": 0
+  }).append(`<h3>${questionBank[questionNumber].choices[0]}`);
+  $(".choice2").attr({
+    "data-index": 1
+  }).append(`<h3>${questionBank[questionNumber].choices[1]}`);
+  $(".choice3").attr({
+    "data-index": 2
+  }).append(`<h3>${questionBank[questionNumber].choices[2]}`);
+  $(".choice4").attr({
+    "data-index": 3
+  }).append(`<h3>${questionBank[questionNumber].choices[3]}`);
+  //click function to stop timer and go to next screen
+  $(".choice1, .choice2, .choice3, .choice4").on("click", function () {
+    choiceSelected = $(this).data("index");
+    clearInterval(intervalId);
+    messageScreen();
+  })
+}
+
+function messageScreen() {
+  
 }
 
 
-var triviaEnd = false;
 
-function triviaEnd() {
 
-}
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------questionScreen function--------------------------------
+
+
 
 // //function end screen
 // function endscreen
@@ -140,3 +184,13 @@ function triviaEnd() {
 // * Don't let the player pick more than one answer per question.
 
 // * Don't forget to include a countdown timer
+//-------------------------------------------fail loop function------------------------
+// for (var i = 0; i < 3; i++) {
+//   var questionChoices = $("<div>");
+//   questionChoices.text(questionbank[questionNumber].choices[i]);
+
+//   questionChoices.attr({
+//     "data-index": i
+//   });
+//   questionChoices.addclass("choicesJS");
+//   $(".choices").append(questionChoices);
