731aaa9079aeea4e6cae10706c84fe11d022c47d
Priyesh <91priyesh@gmail.com>
2018-12-26 20:44:06 -0500

    new js code, created arrays, created functions for start and end triva

--- assets/javascript/appRework.js
+++ assets/javascript/appRework.js
@@ -2,35 +2,51 @@ var correct = 0;
 var incorrect = 0;
 var noAnswer = 0;
 var questionNumber = 0;
-var choiceSelected;
+var startScreen;
 var answered;
 var correctAns;
 var incorrectAns;
-var noAns;
-var endRandom = 0;
+var timer;
+var choiceSelected;
+var time = 12
+
 
 var questionBank = [
-  "What is the name of the fictional reclusive African republic that T’challa, a.k.a Black Panther, hails from?",
-  "Dr. Stephen Strange protects the New York Sanctum. Where are the other sanctums located?", 
-  "What year was the first Iron Man movie released, kicking off the Marvel Cinematic Universe?", 
-  "Which Infinity Stone does Vision have embedded into his forehead?",
-  "Which Avenger does Thor team up with in Thor: Ragnarok?"
+  "1) What is the name of the fictional reclusive African republic that T’challa, a.k.a Black Panther, hails from?",
+  "2)Dr. Stephen Strange protects the New York Sanctum. Where are the other sanctums located?", 
+  "3)What year was the first Iron Man movie released, kicking off the Marvel Cinematic Universe?", 
+  "4)Which Infinity Stone does Vision have embedded into his forehead?",
+  "5)Which Avenger does Thor team up with in Thor: Ragnarok?"
 ];
 
 var choiceBank = [
-  ["Wakanda", "Agamotto", "Sakovia", "Terra"],
-  ["Tokyo & Paris", "London & Hong Kong", "London & Beijing", "Rome & Kathmandu"],
-  ["2008", "2004", "2012", "2006"],
-  ["The Time Stone", "The Soul Stone", "The Mind Stone", "The Space Stone"],
-  ["Ant Man", "Iron Man", "Hawkeye", "Hulk"]
+  ["A. Wakanda", "B. Agamotto", "C. Sakovia", "D. Terra"],
+  ["A. Tokyo & Paris", "B. London & Hong Kong", "C. London & Beijing", "D. Rome & Kathmandu"],
+  ["A. 2008", "B. 2004", "C. 2012", "D. 2006"],
+  ["A. The Time Stone", "B. The Soul Stone", "C. The Mind Stone", "D. The Space Stone"],
+  ["A. Ant Man", "B. Iron Man", "C. Hawkeye", "D. Hulk"]
 ];
 
 var answerBank = [
-  "Wakanda",
-  "London & Hong Kong",
-  "2008",
-  "The Mind Stone",
-  "Hulk"
+  "A. Wakanda",
+  "B. London & Hong Kong",
+  "A. 2008",
+  "C. The Mind Stone",
+  "D. Hulk"
 ];
+
+//function to create start button start and hide endtriva button.
+function startButton(){
+  $("#startTrivia").on("click", function () {
+    $(this).hide();
+    blankScreen();
+  });
+  $("#resetTrivia").hide();
+}
+//function to show endtrivia button
+function endButton(){
+  $("#resetTrivia").show();
+}
+
 
 
